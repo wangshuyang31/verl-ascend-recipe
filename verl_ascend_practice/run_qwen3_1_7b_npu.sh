8eb547871bf8fcda5ba77cd3c7d2b916e0da7504
wangshuyang31 <wangshuyang8@huawei.com>
2026-05-14 11:14:31 +0800

    Create run_qwen3_1_7b_npu.sh

---
+++ verl_ascend_practice/run_qwen3_1_7b_npu.sh
@@ -0,0 +1,64 @@
+set -x
+ENGINE=${1:-vllm}
+
+FSDP_SIZE=${FSDP_SIZE:-4}
+SP_SIZE=${SP_SIZE:-2}
+EP_SIZE=${EP_SIZE:-1}
+NUM_GPUS=${NUM_GPUS:-16}
+
+TRAIN_FILE=dapo-math-17k.parquet
+TEST_FILE=aime-2024.parquet
+max_prompt_length=$((1024 * 2))
+max_response_length=$((1024 * 6))
+
+python3 -m verl.trainer.main_ppo \
+    model_engine=veomni \
+    algorithm.adv_estimator=grpo \
+    data.train_files="${TRAIN_FILE}" \
+    data.val_files="${TEST_FILE}" \
+    data.train_batch_size=128 \
+    data.max_prompt_length=${max_prompt_length} \
+    data.max_response_length=${max_response_length} \
+    data.filter_overlong_prompts=False \
+    data.truncation='error' \
+    actor_rollout_ref.model.path=Qwen3-1.7B \
+    actor_rollout_ref.actor.optim.lr=5e-7 \
+    actor_rollout_ref.model.use_remove_padding=True \
+    actor_rollout_ref.model.enable_gradient_checkpointing=True \
+    actor_rollout_ref.actor.veomni.param_offload=True \
+    actor_rollout_ref.actor.veomni.optimizer_offload=True \
+    actor_rollout_ref.actor.ppo_mini_batch_size=64 \
+    actor_rollout_ref.actor.ppo_micro_batch_size_per_gpu=1 \
+    actor_rollout_ref.actor.use_kl_loss=True \
+    actor_rollout_ref.actor.kl_loss_coef=0.001 \
+    actor_rollout_ref.actor.kl_loss_type=low_var_kl \
+    actor_rollout_ref.actor.entropy_coeff=0 \
+    actor_rollout_ref.actor.use_torch_compile=False \
+    actor_rollout_ref.actor.veomni.fsdp_size="${FSDP_SIZE}" \
+    actor_rollout_ref.actor.veomni.ulysses_parallel_size="${SP_SIZE}" \
+    actor_rollout_ref.actor.veomni.expert_parallel_size="${EP_SIZE}" \
+    actor_rollout_ref.ref.log_prob_micro_batch_size_per_gpu=1 \
+    actor_rollout_ref.ref.veomni.param_offload=True \
+    actor_rollout_ref.ref.use_torch_compile=False \
+    actor_rollout_ref.rollout.log_prob_micro_batch_size_per_gpu=1 \
+    actor_rollout_ref.rollout.max_num_batched_tokens=$((1024)) \
+    actor_rollout_ref.rollout.tensor_model_parallel_size=2 \
+    actor_rollout_ref.rollout.enable_chunked_prefill=False \
+    actor_rollout_ref.rollout.name=vllm \
+    actor_rollout_ref.rollout.gpu_memory_utilization=0.8 \
+    actor_rollout_ref.rollout.free_cache_engine=True \
+    actor_rollout_ref.rollout.enforce_eager=False \
+    actor_rollout_ref.rollout.n=4 \
+    actor_rollout_ref.ref.veomni.optimizer_offload=True \
+    algorithm.kl_ctrl.kl_coef=0.001 \
+    trainer.use_legacy_worker_impl=disable \
+    trainer.critic_warmup=0 \
+    trainer.logger=console \
+    trainer.project_name='verl_veomni_test' \
+    trainer.n_gpus_per_node="${NUM_GPUS}" \
+    trainer.nnodes=1 \
+    trainer.device=npu \
+    trainer.save_freq=100 \
+    trainer.test_freq=-1 \
+    trainer.total_epochs=1 \
+    trainer.total_training_steps=100
